a0c4619e3ee872479610bf167cbaf06346522660
Rasheed <rasheedhazard8@gmail.com>
2022-09-09 00:07:36 -0700

    4 and 5 task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print some text
+ *
+ * Return:Always 0(worked)
+ */
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n")
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - print variable sizes
+*
+* Return:Always 0 (works)
+*/
+
+int main(void)
+{
+    char a;
+    int b;
+    long int c;
+    long long int d;
+    float e;
+
+
+    
+}
